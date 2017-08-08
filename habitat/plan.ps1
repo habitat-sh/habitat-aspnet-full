8bd0ea73026e8dd61d9caaf1af8379d30af425a5
Matt Wrock <matt@mattwrock.com>
2017-08-08 10:16:15 -0700

    reference 4.7 dev pack since that is safer if 4.7 features are used
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -7,7 +7,8 @@ $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('MIT')
 $pkg_description="A sample ASP.NET Full FX IIS app"
 $pkg_build_deps=@(
-  "core/nuget"
+  "core/nuget",
+  "core/dotnet-47-dev-pack"
 )
 
 function invoke-download { }
@@ -17,6 +18,7 @@ function Invoke-Build {
   Copy-Item $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force -Exclude ".vagrant"
   nuget restore $HAB_CACHE_SRC_PATH/$pkg_dirname/packages.config -PackagesDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/packages -Source "https://www.nuget.org/api/v2"
   nuget install MSBuild.Microsoft.VisualStudio.Web.targets -Version 14.0.0.3 -OutputDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/
+  $env:TargetFrameworkRootPath="$(Get-HabPackagePath dotnet-47-dev-pack)\Program Files\Reference Assemblies\Microsoft\Framework"
   $env:VSToolsPath = "$HAB_CACHE_SRC_PATH/$pkg_dirname/MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3/tools/VSToolsPath"
   ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" $HAB_CACHE_SRC_PATH/$pkg_dirname/habitat-aspnet-full.csproj /t:Build /p:VisualStudioVersion=14.0
   if($LASTEXITCODE -ne 0) {
