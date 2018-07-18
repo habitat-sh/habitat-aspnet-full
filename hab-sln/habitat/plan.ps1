a1070b2e89d40376d2c13f83f4fcbdea448542ef
mwrock <matt@mattwrock.com>
2018-07-17 20:33:08 -0700

    use vs 2017 msbuild tooling
    Signed-off-by: mwrock <matt@mattwrock.com>

--- hab-sln/habitat/plan.ps1
+++ hab-sln/habitat/plan.ps1
@@ -7,7 +7,8 @@ $pkg_description="A sample ASP.NET Full FX IIS app"
 $pkg_deps=@("core/dsc-core")
 $pkg_build_deps=@(
   "core/nuget",
-  "core/dotnet-47-dev-pack"
+  "core/dotnet-47-dev-pack",
+  "core/visual-build-tools-2017"
 )
 
 function Invoke-Build {
@@ -16,12 +17,12 @@ function Invoke-Build {
   nuget install MSBuild.Microsoft.VisualStudio.Web.targets -Version 14.0.0.3 -OutputDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/
   $env:TargetFrameworkRootPath="$(Get-HabPackagePath dotnet-47-dev-pack)\Program Files\Reference Assemblies\Microsoft\Framework"
   $env:VSToolsPath = "$HAB_CACHE_SRC_PATH/$pkg_dirname/MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3/tools/VSToolsPath"
-  ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:Build /p:VisualStudioVersion=14.0
+  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:Build
   if($LASTEXITCODE -ne 0) {
       Write-Error "dotnet build failed!"
   }
 }
 
 function Invoke-Install {
-  ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:WebPublish /p:WebPublishMethod=FileSystem /p:publishUrl=$pkg_prefix/www
+  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:WebPublish /p:WebPublishMethod=FileSystem /p:publishUrl=$pkg_prefix/www
 }
