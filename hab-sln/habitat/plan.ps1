c6c3be96bbad269cfbcabf7d7f087ec214e79d01
mwrock <matt@mattwrock.com>
2020-09-25 14:01:43 -0700

    simplify and build with vs2019
    Signed-off-by: mwrock <matt@mattwrock.com>

--- hab-sln/habitat/plan.ps1
+++ hab-sln/habitat/plan.ps1
@@ -11,9 +11,8 @@ $pkg_deps=@(
   "core/dsc-core"
 )
 $pkg_build_deps=@(
-  "core/nuget",
   "core/dotnet-472-dev-pack",
-  "core/visual-build-tools-2017"
+  "mwrock/visual-build-tools-2019/16.7.3/20200925115314"
 )
 $pkg_exposes=@('port')
 $pkg_exports=@{
@@ -22,16 +21,13 @@ $pkg_exports=@{
 
 function Invoke-Build {
   Copy-Item $PLAN_CONTEXT/../../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force
-  nuget restore $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/packages.config -PackagesDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/packages -Source "https://www.nuget.org/api/v2"
-  nuget install MSBuild.Microsoft.VisualStudio.Web.targets -Version 14.0.0.3 -OutputDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/
-  $env:TargetFrameworkRootPath="$(Get-HabPackagePath dotnet-472-dev-pack)\Program Files\Reference Assemblies\Microsoft\Framework"
-  $env:VSToolsPath = "$HAB_CACHE_SRC_PATH/$pkg_dirname/MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3/tools/VSToolsPath"
-  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:Build
+  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:Build /restore
   if($LASTEXITCODE -ne 0) {
       Write-Error "dotnet build failed!"
   }
 }
 
 function Invoke-Install {
-  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:WebPublish /p:WebPublishMethod=FileSystem /p:publishUrl=$pkg_prefix/www
+  # $env:VSToolsPath = "$(Get-HabPackagePath visual-build-tools-2019)\Contents\MSBuild\Microsoft\VisualStudio\v16.0"
+  MSBuild $HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name/${pkg_name}.csproj /t:WebPublish /p:WebPublishMethod=FileSystem /p:publishUrl=$pkg_prefix/www # /v:diag > c:\src\log.txt
 }
